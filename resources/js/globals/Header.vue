2dcfe39a3db874517575e2862605281a02f54cb4
lnoueryo <popo62520908@gmail.com>
2021-02-14 11:14:26 +0900

    ヘッダーを2つに分割

---
+++ resources/js/globals/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <v-img src="/images/headermain.png"></v-img>
+        <nav>
+            <div class="d-flex justify-content-around" style="font-weight: bold; border: solid 3px; border-color: #ebdedaa3; background-color: #e9967a87;padding:20px">
+                <div v-for="(item, i) in items" :key="i">
+                    <router-link :to="item.route" exact-active-class="active">{{ item.page }}</router-link>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {page: 'トップ', route: '/home'},
+                {page: 'コンセプト', route: '/concept'},
+                {page: 'メニュー', route: '/menu'},
+                {page: 'Web予約', route: '/booking'},
+                {page: 'ブログ', route: '/blog'},
+                {page: 'ログイン', route: '/'},
+                {page: '会員登録', route: '/register'},
+            ]
+        }
+    },
+}
+</script>
+<style>
+    .active{
+        color: aliceblue;
+        background-color: black;
+    }
+</style>
---
+++ resources/js/globals/Header2.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="a">
+        <v-img src="/images/inside.png"></v-img>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<style scoped>
+    .fade-enter-active, .fade-leave-active {
+        will-change: opacity;
+        transition: opacity 1000ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    }
+    .fade-enter, .fade-leave-to {
+        opacity: 0
+    }
+
+</style>
