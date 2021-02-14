5090b0b324ac5f1cbb322fc8d7a4e336993f80b9
lnoueryo <popo62520908@gmail.com>
2021-02-14 11:13:55 +0900

    名前付きビューに変更

---
+++ resources/js/Index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <transition name="fade" mode="out-in">
+            <router-view name="header"></router-view>
+        </transition>
+        <transition name="fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
+    </div>
+</template>
